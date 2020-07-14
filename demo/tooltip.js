d67f130274c947e139865675b1553f037ff1dc71
hudeyong <hudeyong@icsoc.net>
2020-07-14 09:49:16 +0800

    js控件例子

---
+++ demo/tooltip.js
@@ -0,0 +1,5 @@
+<i class="icon-question4" data-html="true" data-popup="tooltip" title="待付款：下单未付款订单；<br/>待取货：付款未到店提货"></i>
+
+<script type="text/javascript">
+$('[data-popup="tooltip"]').tooltip();
+</script>
